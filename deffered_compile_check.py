5e4bf8e7a4be0fb0198dc03dffabbc41d95f52ce
Petar Stoyanov <pesho.stoyanov@gmail.com>
2018-01-10 10:50:53 +0200

    Create deffered_compile_check.py

---
+++ deffered_compile_check.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+
+def main():
+    if name == 'Guido':
+        print repeeeet(name) + '!!!' # program will compile and work until the name happens to be Guido
+    else:
+        print repeat(name)
+
+if __name__ == '__main__':
+    main()
