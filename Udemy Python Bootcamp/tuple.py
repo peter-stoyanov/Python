7a30b6db340e76efdc5f2f24c3449a83b6387dad
Petar Stoyanov <pesho.stoyanov@gmail.com>
2018-01-29 17:04:54 +0200

    basic regex

---
+++ Udemy Python Bootcamp/tuple.py
@@ -0,0 +1,16 @@
+import statistics
+
+#!/usr/bin/env python
+"""Docstring"""
+
+def main():
+    """Docstring"""
+
+    def example():
+        return 15, 12, 11
+
+    x, y, z = example()
+    print(x, y, z)
+
+if __name__ == '__main__':
+    main()
